441e20fd34523df2b8f9d5716c340ebc56510c4c
C Nelson <cnelsonsic@gmail.com>
2011-12-17 22:24:37 -0600

    More changes

--- authserver.py
+++ authserver.py
@@ -1,15 +1,67 @@
 #!/usr/bin/env python
+'''AuthServer
+A server providing authentication, and allows a user to get a list of their characters.
+'''
 
-class AuthServer(object):
-    '''AuthServer authenticates a user and redirects them to the appropriate ZoneServer.'''
+# TODO: Write a function to pull in the docstrings from defined classes here and 
+# append them to the module docstring
+
+import json
+
+import tornado
+from tornado.web import RequestHandler
+
+from settings import AUTHSERVERPORT
+
+from baseserver import BaseServer, SimpleHandler
+from require_basic_auth import require_basic_auth
+
+class PingHandler(RequestHandler):
+    def get(self):
+        self.write("pong")
+
+@require_basic_auth
+class AuthHandler(RequestHandler):
+    '''AuthHandler authenticates a user and sets a session in the database.'''
+
+    def get(self, basicauth_user, basicauth_pass):
+        self.authenticate(basicauth_user, basicauth_pass)
+        # TODO: Write a sessionId to the database and return it
+
+    def post(self, **kwargs):
+        basicauth_user = kwargs['basicauth_user']
+        basicauth_pass = kwargs['basicauth_pass']
 
     def authenticate(self, username, password):
         '''Compares a username/password pair against that in the database.
         If they match, return True.
         Else, return False.'''
+        return True
 
-    def get_zoneserver(self, character):
-        '''Takes a character reference and gets the zoneserver that they should
-        be redirected to.
-        Returns a URL for that zoneserver.'''
+@require_basic_auth
+class CharacterHandler(RequestHandler):
+    '''CharacterHandler gets a list of characters for the given user account.'''
 
+    def get(self, basicauth_user, basicauth_pass):
+        self.write(json.dumps(self.get_characters(basicauth_user)))
+
+    def post(self, **kwargs):
+        basicauth_user = kwargs['basicauth_user']
+        basicauth_pass = kwargs['basicauth_pass']
+
+    def get_characters(self, username):
+        '''Queries the database for all characters owned by a particular username.'''
+        return ['Graxnor', 'Rumtiddlykins']
+
+if __name__ == "__main__":
+    handlers = []
+    handlers.append((r"/", lambda x, y: SimpleHandler(__doc__, x, y)))
+    handlers.append((r"/ping", PingHandler))
+    handlers.append((r"/authenticate", AuthHandler))
+    handlers.append((r"/characters", CharacterHandler))
+
+    server = BaseServer(handlers)
+    server.listen(AUTHSERVERPORT)
+
+    print "Starting up Authserver..."
+    server.start()
--- idealclient.py
+++ idealclient.py
@@ -1,27 +1,132 @@
 '''This is an example implementation of an ideal client.
 It won't work and probably won't even execute.'''
 
+import exceptions
+from time import sleep
+import json
+
+import requests
+
+from settings import *
+
+# Some settings:
+USERNAME = "user"
+PASSWORD = "pass"
+DEBUG = True
+
+# What hostname are the servers on
+PREFIX = "http://" # FIXME: Use proper name.
+HOSTNAME = "localhost"
+
+# Server URLs
+AUTHSERVER = "%s%s:%d" % (PREFIX, HOSTNAME, AUTHSERVERPORT)
+CHARSERVER = "%s%s:%d" % (PREFIX, HOSTNAME, CHARSERVERPORT)
+ZONESERVER = "%s%s:%d" % (PREFIX, HOSTNAME, MASTERZONESERVERPORT)
+
+class ConnectionError(exceptions.Exception):
+    def __init__(self, param):
+        self.param = param
+        return
+    def __str__(self):
+        return repr(self.param)
+
 # Ping main server to make sure it's up, and really an authserver
+def ping_authserver():
+    r = requests.get(''.join((AUTHSERVER, "/ping")))
+    content = r.content
+
+    if content != "pong":
+        raise ConnectionError("Something went wrong." % content)
+    else:
+        return True
 
 # Send username/password credentials to server
 # Server sends back a cookie that we send with all our requests.
+def authenticate(username, password):
+    r = requests.get(''.join((AUTHSERVER, "/authenticate")), auth=(username, password))
+    content = r.content
 
-# Ask charserver for list of characters associated with this account
-# charserver returns a list of characters and their locations.
+    if r.status_code == 200:
+        return True
+    else:
+        return r
+
+# Ask authserver for list of characters associated with this account
+# authserver returns a list of characters
+def get_characters(username, password):
+    r = requests.get(''.join((AUTHSERVER, "/characters")), auth=(username, password))
+    if r.status_code == 200:
+        return json.loads(r.content)
+    else:
+        return r
+
 # We could also query any details about the character like inventory 
 #   or money or current stats at this point
 
 # We pick a character we want to play and query the charserver for its current zone
-# charserver returns the zoneserver url
+# charserver returns the zone id, which uniquely identifies it persistently.
+def get_zone(charname):
+    r = requests.get(''.join((CHARSERVER, '/', charname, '/zone')), )
+    if r.status_code == 200:
+        return json.loads(r.content).get('zone')
+    else:
+        return r
 
-# We send a request to the zoneserver to mark our character as online/active
-# Send an initial movement message to the zoneserver's movement handler to open the connection
+# We then send a request to the master zone server for the url to the given zone
+# If it's online already, send us the URL
+# If it's not online, spin one up and send it when ready.
+def get_zoneserver(username, password, zone):
+    r = requests.get(''.join((ZONESERVER, '/', zone)), auth=(username, password))
+    if r.status_code == 200:
+        return r.content
+    else:
+        return r
 
 # Request all objects in the zone. (Terrain, props, players are all objects)
 # Bulk of loading screen goes here while we download object info.
+def get_all_objects(zone, username, password):
+    r = requests.get(''.join((zone, '/objects')), auth=(username, password))
+    if r.status_code == 200:
+        return json.loads(r.content)
+    else:
+        return r
+
+# We send a request to the zoneserver to mark our character as online/active
+# Send an initial movement message to the zoneserver's movement handler to open the connection
 
 ### Repetitive ###
 # Every second or so, request objects that have changed or been added since the last request.
 # Send movement keys to the movement handler of the zoneserver as fast as possible to move the character about
 
+if __name__ == "__main__":
 
+    # Try to ping the authserver
+    # If connecting fails, wait longer each time
+    sleeptime = 2
+    authserver_exists = False
+    while not authserver_exists:
+        try:
+            authserver_exists = ping_authserver()
+            if authserver_exists:
+                break # If we connect succesfully, break out of the while
+        except Exception, exc:
+            # ConnectionErrors mean we failed to connect, so retry.
+            if DEBUG:
+                print "Connecting failed:", exc
+
+        print "Reconnecting in %.01f seconds..." % sleeptime
+        sleep(sleeptime)
+        sleeptime = sleeptime**1.5
+
+    # Since the server is up, authenticate.
+    if authenticate(USERNAME, PASSWORD) is True:
+        print "authenticated"
+
+    chars = get_characters(USERNAME, PASSWORD)
+    print "Got %r as characters." % chars
+
+    zone = get_zone(chars[0])
+    print "Got %r as zone." % zone
+
+    zoneserver = get_zoneserver(USERNAME, PASSWORD, zone)
+    print "Got %s as zone url." % zoneserver
