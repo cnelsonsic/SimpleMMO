7c6da05f350c4a1776273ec8202f9c5db3f1c6ec
C Nelson <cnelsonsic@gmail.com>
2011-12-17 23:02:57 -0600

    Adding charserver and settings, as well as basic http auth decorator.

---
+++ charserver.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''CharServer
+A server for retrieving information about characters.
+For example, getting which zone a character is currently in.
+'''
+
+# TODO: Write a function to pull in the docstrings from defined classes here and 
+# append them to the module docstring
+
+import json
+
+import tornado
+from tornado.web import RequestHandler
+
+from settings import CHARSERVERPORT
+
+from baseserver import BaseServer, SimpleHandler
+from require_basic_auth import require_basic_auth
+
+
+class CharacterZoneHandler(RequestHandler):
+    '''CharacterHandler gets information for a given character.'''
+
+    def get(self, character):
+        self.write(json.dumps(self.get_zone(character)))
+
+    def get_zone(self, charname):
+        '''Queries the database for information pertaining directly
+        to the character.
+        It currently only returns the zone the character is in.'''
+        return {'zone':'playerinstance-GhibliHills-%s' % (charname,)}
+
+if __name__ == "__main__":
+    handlers = []
+    handlers.append((r"/", lambda x, y: SimpleHandler(__doc__, x, y)))
+    handlers.append((r"/(.*)/zone", CharacterZoneHandler))
+
+    server = BaseServer(handlers)
+    server.listen(CHARSERVERPORT)
+
+    print "Starting up Charserver..."
+    server.start()
---
+++ require_basic_auth.py
@@ -0,0 +1,25 @@
+
+import base64
+
+# From http://kelleyk.com/post/7362319243/easy-basic-http-authentication-with-tornado
+def require_basic_auth(handler_class):
+    def wrap_execute(handler_execute):
+        def require_basic_auth(handler, kwargs):
+            auth_header = handler.request.headers.get('Authorization')
+            if auth_header is None or not auth_header.startswith('Basic '):
+                handler.set_status(401)
+                handler.set_header('WWW-Authenticate', 'Basic realm=Restricted')
+                handler._transforms = []
+                handler.finish()
+                return False
+            auth_decoded = base64.decodestring(auth_header[6:])
+            kwargs['basicauth_user'], kwargs['basicauth_pass'] = auth_decoded.split(':', 2)
+            return True
+        def _execute(self, transforms, *args, **kwargs):
+            if not require_basic_auth(self, kwargs):
+                return False
+            return handler_execute(self, transforms, *args, **kwargs)
+        return _execute
+
+    handler_class._execute = wrap_execute(handler_class._execute)
+    return handler_class
