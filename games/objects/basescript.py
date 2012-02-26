2f1423eb57c21938ce85a07e3a3760901f2a852a
C Nelson <cnelsonsic@gmail.com>
2012-02-26 16:29:42 -0600

    Add support for rolling dice for easy random number generation in scripts.

--- games/objects/basescript.py
+++ games/objects/basescript.py
@@ -3,6 +3,19 @@ import datetime
 
 from mongoengine_models import Message
 
+import os
+import re
+import random
+
+from settings import MAX_DICE_AMOUNT
+
+def parse(s):
+    result = re.search(r'^((?P<rolls>\d+)#)?(?P<dice>\d*)d(?P<sides>\d+)(?P<mod>[+-]\d+)?$', s)
+    return (result.group('rolls') or 0,
+            result.group('dice') or 1,
+            result.group('sides') or 2,
+            result.group('mod') or 0)
+
 class Script(object):
     '''This is a placeholder class used for doing object script things.
     It's mostly just used for detecting if an object is really a script or not.
@@ -10,6 +23,24 @@ class Script(object):
     def __init__(self, mongo_engine_object=None):
         self.me_obj = mongo_engine_object
         print "Initted with %s" % self.me_obj
+
+    def roll(self, dicestring):
+        '''Roll dice specified by dicestring and return the value.
+        Does not allow more dice than the MAX_DICE_AMOUNT setting
+        to be rolled at once.'''
+
+        rolls, num, sides, mod = parse(dicestring)
+        num = int(num)
+        sides = int(sides)
+        mod = int(mod)
+        if num > MAX_DICE_AMOUNT:
+            raise UserWarning("Cannot roll more than %d dice at once." % MAX_DICE_AMOUNT)
+
+        rolls = []
+        for i in xrange(num):
+            #print "Rolling dice #%d of %d: %f%% done." % (i, num, (float(i)/float(num))*100)
+            rolls.append(random.randrange(1, int(sides)+1)+int(mod))
+        return sum(rolls)
 
     def say(self, message):
         '''Write the given text to the zone message database.'''
