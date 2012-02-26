bc2ee24fa28d7932ad4ea7c19c7005d4f018deeb
C Nelson <cnelsonsic@gmail.com>
2012-02-26 16:30:16 -0600

    Chickens now move randomly sometimes.

--- games/objects/chicken.py
+++ games/objects/chicken.py
@@ -1,8 +1,8 @@
 
 from games.objects.basescript import Script
 
-
 import random
+import datetime
 
 class Chicken(Script):
     def __init__(self, *args, **kwargs):
@@ -10,6 +10,13 @@ class Chicken(Script):
         super(Chicken, self).__init__(*args, **kwargs)
 
     def tick(self):
-        if random.randint(1, 100) > 95:
+        if self.roll("1d100") == 100:
             self.say(random.choice(('BAWK', 'BUCAW', 'BUCK BUCK BUCK', 'BROOOOCK', 'CLUCK')))
+
+        if self.roll("1d100") == 100:
+            # Move around randomly.
+            self.say("I think I'll take a stroll!")
+            self.me_obj.loc.x += random.randint(-1, 1)
+            self.me_obj.loc.y += random.randint(-1, 1)
+            self.me_obj.set_modified()
 
