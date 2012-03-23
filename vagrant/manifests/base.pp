67ad9c330204189e960fce44b93598cf40970e72
C Nelson <cnelsonsic@gmail.com>
2012-03-22 19:18:46 -0500

    Adding some packages to the puppet script.

--- vagrant/manifests/base.pp
+++ vagrant/manifests/base.pp
@@ -3,3 +3,14 @@ group { "puppet":
   ensure => "present",
 }
 
+package { [
+    'python-setuptools',
+    'python-dev',
+    'build-essential',
+    'sqlite-3',
+    'mongodb',
+    ]:
+    ensure => 'latest';
+}
+
+
