96cc27bbbfc0379ac8e19172d30b7c6c0337b602
C Nelson <cnelsonsic@gmail.com>
2012-03-23 23:35:44 -0500

    Adding initial charserver tests.

---
+++ tests/test_charserver.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2.7
+# ##### BEGIN AGPL LICENSE BLOCK #####
+# This file is part of SimpleMMO.
+#
+# Copyright (C) 2011, 2012  Charles Nelson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ##### END AGPL LICENSE BLOCK #####
+
+import unittest
+from tornado.web import Application
+from tornado.testing import AsyncHTTPTestCase
+
+import json
+from mock import Mock
+
+import sys
+sys.path.append(".")
+
+from elixir_models import metadata, setup_all, create_all
+
+import settings
+from charserver import CharacterZoneHandler
+
+def set_up_db():
+    '''Connects to an in-memory SQLite database,
+    with the purpose of emptying it and recreating it.'''
+    metadata.bind = "sqlite:///:memory:"
+#     metadata.bind.echo = True
+    setup_all()
+    create_all()
+
+class TestCharacterZoneHandler(AsyncHTTPTestCase):
+    def get_app(self):
+        return Application([('/(.*)/zone', CharacterZoneHandler)], cookie_secret=settings.COOKIE_SECRET)
+
+    def setUp(self):
+        super(TestCharacterZoneHandler, self).setUp()
+        set_up_db()
+        app = Application([('/(.*)/zone', CharacterZoneHandler)], cookie_secret=settings.COOKIE_SECRET)
+        req = Mock()
+        req.cookies = {}
+        self.character_zone_handler = CharacterZoneHandler(app, req)
+
+    def test_get(self):
+        charname = "testcharname"
+        result = json.loads(self.fetch('/%s/zone' % charname).body)
+        expected = {'zone': 'playerinstance-GhibliHills-%s' % charname}
+        self.assertEqual(result, expected)
+
+    def test_get_zone(self):
+        character = "testcharname"
+        result = self.character_zone_handler.get_zone(character)
+        expected = {'zone': 'playerinstance-GhibliHills-%s' % character}
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
