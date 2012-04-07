a0a430b1672b5821637272b6c278759d354fbd47
C Nelson <cnelsonsic@gmail.com>
2012-04-07 13:53:37 -0500

    Chicken uses the helpers now.

--- games/objects/chicken.py
+++ games/objects/chicken.py
@@ -1,22 +1,24 @@
 
 from games.objects.basescript import Script
 
-import random
-import datetime
-
 class Chicken(Script):
     def __init__(self, *args, **kwargs):
         # Overriding so people remember to do this on their scripts.
         super(Chicken, self).__init__(*args, **kwargs)
 
+        self.idle_chat = ('BAWK', 'BUCAW', 'BUCK BUCK BUCK', 'BROOOOCK', 'CLUCK')
+        self.collide_chat = ("bump.", "bamp!", "BUMP", "THUD!!")
+
     def tick(self):
         if self.roll("1d100") == 100:
-            self.say(random.choice(('BAWK', 'BUCAW', 'BUCK BUCK BUCK', 'BROOOOCK', 'CLUCK')))
+            self.rand_say(self.idle_chat)
 
         if self.roll("1d100") == 100:
             # Move around randomly.
             self.say("I think I'll take a stroll!")
-            self.me_obj.loc.x += random.randint(-1, 1)
-            self.me_obj.loc.y += random.randint(-1, 1)
-            self.me_obj.set_modified()
+            r = self.wander()
+            if not r:
+                self.rand_say(self.collide_chat)
+            else:
+                self.say("That stroll was nice.")
 
