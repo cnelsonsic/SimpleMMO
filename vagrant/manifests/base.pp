0cd1b6685a707306b62e1a8602f2f2c371b97cee
C Nelson <cnelsonsic@gmail.com>
2012-05-19 01:49:51 -0500

    Get the puppet manifest to where everything is installed.

--- vagrant/manifests/base.pp
+++ vagrant/manifests/base.pp
@@ -3,14 +3,32 @@ group { "puppet":
   ensure => "present",
 }
 
+# Install all our required packages.
 package { [
+    'git',
+    'python-pip',
     'python-setuptools',
     'python-dev',
     'build-essential',
-    'sqlite-3',
+    'sqlite3',
     'mongodb',
     ]:
     ensure => 'latest';
 }
 
+# Pull down the source
+exec { 'git clone git://github.com/cnelsonsic/SimpleMMO.git':
+    cwd     => '/home/vagrant',
+    path    => $::path,
+    creates => '/home/vagrant/SimpleMMO/',
+    refresh => 'cd /home/vagrant/SimpleMMO/; git pull',
+    require => Package['git'],
+}
 
+# Install the requirements.
+exec {'pip install --timeout=1 --use-mirrors -r requirements.txt':
+    path    => $::path,
+    cwd     => '/home/vagrant/SimpleMMO',
+    tries   => 5, # Try really hard to install all the pip packages.
+    require => [Package['python-pip'], Package['python-setuptools'], Package['python-dev'], Package['build-essential']],
+}
