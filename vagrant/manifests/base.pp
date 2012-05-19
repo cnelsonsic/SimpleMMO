753c08e6d28236bff59e0c1364e8cafe449feb1d
C Nelson <cnelsonsic@gmail.com>
2012-05-19 04:40:56 -0500

    More updates to make pip not take ages. I hope.

--- vagrant/manifests/base.pp
+++ vagrant/manifests/base.pp
@@ -16,6 +16,14 @@ package { [
     ensure => 'latest';
 }
 
+# Stop the mongod service, SimpleMMO starts its own.
+service { 'mongodb':
+    name    => 'mongod',
+    enable  => 'false',
+    ensure  => 'stopped',
+    require => Package['mongodb'],
+}
+
 # Pull down the source
 exec { 'git clone git://github.com/cnelsonsic/SimpleMMO.git':
     cwd     => '/home/vagrant',
@@ -26,9 +34,13 @@ exec { 'git clone git://github.com/cnelsonsic/SimpleMMO.git':
 }
 
 # Install the requirements.
-exec {'pip install --timeout=1 --use-mirrors -r requirements.txt':
+# TODO: Install these one at a time instead of using the requirements file.
+exec {'pip install --use-mirrors -r requirements.txt':
     path    => $::path,
     cwd     => '/home/vagrant/SimpleMMO',
+    timeout => 0, # Turn off the timeout stuff. It might take longer than 5 minutes.
     tries   => 5, # Try really hard to install all the pip packages.
+    logoutput => true,
+    before  => Service['mongodb'], # Mongo tends to starve out pip. :(
     require => [Package['python-pip'], Package['python-setuptools'], Package['python-dev'], Package['build-essential']],
 }
