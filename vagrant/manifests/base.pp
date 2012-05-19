f41494081755d2e78b809dd0360c0636dcdc6003
C Nelson <cnelsonsic@gmail.com>
2012-05-19 16:37:57 -0500

    Run each pip install as its own exec.
    More work for #1

--- vagrant/manifests/base.pp
+++ vagrant/manifests/base.pp
@@ -18,7 +18,7 @@ package { [
 
 # Stop the mongod service, SimpleMMO starts its own.
 service { 'mongodb':
-    name    => 'mongod',
+    name    => 'mongodb',
     enable  => 'false',
     ensure  => 'stopped',
     require => Package['mongodb'],
@@ -34,13 +34,26 @@ exec { 'git clone git://github.com/cnelsonsic/SimpleMMO.git':
 }
 
 # Install the requirements.
-# TODO: Install these one at a time instead of using the requirements file.
-exec {'pip install --use-mirrors -r requirements.txt':
-    path    => $::path,
-    cwd     => '/home/vagrant/SimpleMMO',
-    timeout => 0, # Turn off the timeout stuff. It might take longer than 5 minutes.
-    tries   => 5, # Try really hard to install all the pip packages.
-    logoutput => true,
-    before  => Service['mongodb'], # Mongo tends to starve out pip. :(
-    require => [Package['python-pip'], Package['python-setuptools'], Package['python-dev'], Package['build-essential']],
+class pip {
+    Exec {
+        path        => $::path,
+        require     => [Package['python-pip'], Package['python-setuptools'], Package['python-dev'], Package['build-essential'], Service['mongodb']],
+        timeout     => 0, # Turn off the timeout stuff. It might take longer than 5 minutes.
+        tries       => 5, # Try really hard to install all the pip packages.
+        logoutput   => 'true',
+    }
+    exec {
+        'pip install --use-mirrors Elixir==0.7.1':;
+        'pip install --use-mirrors sentry==4.2.5':;
+        'pip install --use-mirrors raven==1.7.6':;
+        'pip install --use-mirrors mock==0.8':;
+        'pip install --use-mirrors pymongo==2.1.1':;
+        'pip install --use-mirrors mongoengine==0.6.9':;
+        'pip install --use-mirrors pymunk==2.1.0':;
+        'pip install --use-mirrors requests==0.12.1':;
+        'pip install --use-mirrors supervisor==3.0a12':;
+        'pip install --use-mirrors supervisor_twiddler==0.4':;
+        'pip install --use-mirrors tornado==2.2.1':;
+    }
 }
+include pip
