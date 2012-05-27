40733a9a1858c62a71522221d574e44529264bbf
C Nelson <cnelsonsic@gmail.com>
2012-05-27 12:47:52 -0500

    Use the built in pip package resource.

--- vagrant/manifests/base.pp
+++ vagrant/manifests/base.pp
@@ -37,28 +37,30 @@ exec { 'git clone git://github.com/cnelsonsic/SimpleMMO.git':
 }
 
 # Install the requirements.
-class pip {
-    Exec {
-        path        => $::path,
-        require     => [Package['python-pip'], Package['python-setuptools'], Package['python-dev'], Package['build-essential'], Service['mongodb']],
-        timeout     => 0, # Turn off the timeout stuff. It might take longer than 5 minutes.
-        tries       => 5, # Try really hard to install all the pip packages.
-    }
-    exec {
-        'pip install --use-mirrors Elixir==0.7.1':;
-        'pip install --use-mirrors sentry==4.2.5':;
-        'pip install --use-mirrors raven==1.7.6':;
-        'pip install --use-mirrors mock==0.8':;
-        'pip install --use-mirrors pymongo==2.1.1':;
-        'pip install --use-mirrors mongoengine==0.6.9':;
-        'pip install --use-mirrors pymunk==2.1.0':;
-        'pip install --use-mirrors requests==0.12.1':;
-        'pip install --use-mirrors supervisor==3.0a12':;
-        'pip install --use-mirrors supervisor_twiddler==0.4':;
-        'pip install --use-mirrors tornado==2.2.1':;
-    }
+package {
+    'Elixir':
+        ensure => '0.7.1';
+    'sentry':
+        ensure => '4.2.5';
+    'raven':
+        ensure => '1.7.6';
+    'mock':
+        ensure => '0.8';
+    'pymongo':
+        ensure => '2.1.1';
+    'mongoengine':
+        ensure => '0.6.9';
+    'pymunk':
+        ensure => '2.1.0';
+    'requests':
+        ensure => '0.12.1';
+    'supervisor':
+        ensure => '3.0a12';
+    'supervisor_twiddler':
+        ensure => '0.4';
+    'tornado':
+        ensure => '2.2.1';
 }
-include pip
 
 # Make sure mongodb has a folder
 file { '/home/vagrant/SimpleMMO/mongodb':
@@ -75,13 +77,14 @@ file { '/home/vagrant/SimpleMMO':
 }
 
 # TODO: Copy over sentry's config.
-exec {'sentry init /home/vagrant/.sentry/sentry.conf.py':
+exec { 'sentry init':
+    command     => 'sentry init /home/vagrant/.sentry/sentry.conf.py',
     path        => $::path,
     cwd         => '/home/vagrant/',
     user        => 'vagrant',
     creates     => '/home/vagrant/.sentry/sentry.conf.py',
     logoutput   => 'true',
-    require     => Exec['pip install --use-mirrors sentry==4.2.5'],
+    require     => Package['sentry'],
 }
 
 # Upgrade sentry's database before starting
@@ -91,7 +94,7 @@ exec {'sentry --config=sentry.conf.py upgrade':
     user    => 'vagrant',
     tries   => 5, # Stupid thing tries to ask questions, but it gives up when you run it again.
     logoutput => 'true',
-    require => [Exec['pip install --use-mirrors sentry==4.2.5'], Exec['sentry init /home/vagrant/.sentry/sentry.conf.py']],
+    require => [Package['sentry'], Exec['sentry init']],
     before => Exec['supervisord'],
 }
 
