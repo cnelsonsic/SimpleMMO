4cfac2b5649bbc76c308fd0dde09e29a60350f0f
C Nelson <cnelsonsic@gmail.com>
2012-06-16 10:11:59 -0500

    Rename client.py to clientlib.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,7 +10,7 @@ sys.path.append(".")
 
 import settings
 
-import client
+import clientlib
 
 from integration_base import IntegrationBase
 
@@ -53,23 +53,23 @@ class TestClient(IntegrationBase):
 
     def test___init__(self):
         '''Init the Client with no args.'''
-        c = client.Client()
+        c = clientlib.Client()
         self.assertTrue(c)
 
     def test___init___autoauth(self):
         '''Init the Client with a known good user/pass combo.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(c.cookies)
         self.assertTrue(c.cookies['user'])
 
     def test___init___autoauth_bad(self):
         '''Initting the Client with a bad user/pass combo will raise an exception. '''
-        with self.assertRaises(client.AuthenticationError):
-            client.Client(username="BadUser", password="BadPassword")
+        with self.assertRaises(clientlib.AuthenticationError):
+            clientlib.Client(username="BadUser", password="BadPassword")
 
     def test_register(self):
         '''Register with a good username and password.'''
-        c = client.Client()
+        c = clientlib.Client()
         with self.fresh_servers():
             result = c.register(username="gooduser", password="goodpass", email="test@example.com")
         self.assertTrue(result)
@@ -81,7 +81,7 @@ class TestClient(IntegrationBase):
         should be squelched and all continues as if nothing went awry.
         (If the user re-instantiates the client, show errors as normal.)
         '''
-        c = client.Client()
+        c = clientlib.Client()
         with self.fresh_servers():
             first_result = c.register(username="gooduser", password="goodpass", email="test@example.com")
             self.assertTrue(first_result)
@@ -94,25 +94,25 @@ class TestClient(IntegrationBase):
         '''Register a good username and password twice, from different instances.
         If a user tries to register a username that is already taken, show them
         a meaningful error.'''
-        c = client.Client()
+        c = clientlib.Client()
         with self.fresh_servers():
             result = c.register(username="gooduser", password="goodpass", email="test@example.com")
             self.assertTrue(result)
 
-            c = client.Client()
-            with self.assertRaises(client.RegistrationError) as cm:
+            c = clientlib.Client()
+            with self.assertRaises(clientlib.RegistrationError) as cm:
                 c.register(username="gooduser", password="goodpass", email="test@example.com")
             self.assertIn('User already exists.', cm.exception.message)
 
     def test_ping(self):
         '''Pinging the authserver should work.'''
-        c = client.Client()
+        c = clientlib.Client()
         result = c.ping()
         self.assertTrue(result)
 
     def test_authenticate(self):
         '''Authenticating after initting Client should work.'''
-        c = client.Client()
+        c = clientlib.Client()
         result = c.authenticate(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(result)
         self.assertTrue(c.cookies['user'])
@@ -120,33 +120,33 @@ class TestClient(IntegrationBase):
 
     def test_authenticate_after_auth(self):
         '''Authenticating after initting Client with auth should work.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         result = c.authenticate(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(result)
         self.assertTrue(c.cookies['user'])
 
     def test_authenticate_bad(self):
         '''Authenticating with bad credentials after init does not succeed.'''
-        c = client.Client()
+        c = clientlib.Client()
         result = c.authenticate(username='BadUser', password='BadPassword')
         self.assertFalse(result)
         self.assertFalse(c.cookies)
 
     def test_characters(self):
         '''After authenticating, Client has a dictionary of characters.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(c.characters)
         self.assertIn(self.character, c.characters)
         self.assertTrue(c.characters[self.character])
 
     def test_get_zone(self):
         '''Client can get the zone for our character.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(c.get_zone(self.character))
 
     def test_get_zone_cacheing(self):
         '''Client can get the zone for our character from the cache.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         self.assertTrue(c.get_zone(self.character))
         expected = "Expected Zone"
         c.characters[self.character].zone = expected
@@ -155,20 +155,20 @@ class TestClient(IntegrationBase):
 
     def test_get_zone_url(self):
         '''Client can get the url for our character's zone.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         zoneid = c.get_zone(self.character)
         zoneurl = c.get_zone_url(zoneid)
         self.assertIn('http', zoneurl)
 
     def test_get_zone_url_no_args(self):
         '''Client can get the url for the last character's zone.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         zoneurl = c.get_zone_url()
         self.assertIn('http', zoneurl)
 
     def test_get_zone_url_with_cache(self):
         '''Client can get the url for our character's zone from the cache'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         zoneid = c.get_zone(self.character)
         c.get_zone_url(zoneid)
 
@@ -180,13 +180,13 @@ class TestClient(IntegrationBase):
 
     def test_get_objects(self):
         '''Client can get the objects in our character's zone.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         c.get_objects()
         self.assertTrue(c.objects)
 
     def test_get_objects_verbose(self):
         '''Client can get the objects in our character's zone, when we give it.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         zoneid = c.get_zone(self.character)
         zoneurl = c.get_zone_url(zoneid)
         c.get_objects(zoneurl)
@@ -194,7 +194,7 @@ class TestClient(IntegrationBase):
 
     def test_get_objects_update(self):
         '''Client can update its objects.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         c.get_objects()
         result = c.get_objects()
         self.assertTrue(c.objects)
@@ -202,7 +202,7 @@ class TestClient(IntegrationBase):
 
     def test_set_character_status(self):
         '''Client can update its objects.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         c.get_zone_url(c.get_zone(self.character))
         c.get_objects()
         result = c.set_character_status(self.character, 'online')
@@ -211,7 +211,7 @@ class TestClient(IntegrationBase):
 
     def test_move_character(self):
         '''Client can move a character.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         character = self.character
         c.get_zone(character)
         c.set_character_status(character, 'online')
@@ -221,7 +221,7 @@ class TestClient(IntegrationBase):
 
     def test_move_character_bump(self):
         '''Client can try to move a character, but fail due to physics.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         character = self.character
         c.get_zone(character)
         c.set_character_status(character, 'online')
@@ -230,11 +230,11 @@ class TestClient(IntegrationBase):
         self.assertFalse(result)
 
     def test_main(self):
-        self.assertTrue(client.main(ticks=5))
+        self.assertTrue(clientlib.main(ticks=5))
 
     def test_activation(self):
         '''Client can activate an object.'''
-        c = client.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
+        c = clientlib.Client(username=settings.DEFAULT_USERNAME, password=settings.DEFAULT_PASSWORD)
         character = self.character
         print "testing using ", character
         # Override the character's zone:
